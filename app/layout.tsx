2b366745855c0775d6246bff4c5179792ace7260
JAM <jonathan.ma97@outlook.com>
2024-05-28 10:38:47 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jonathan Ma | Portfolio",
+  description: "Jack of all trades master of none",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className= {`${inter.className} bg-gray-50`}>{children}</body>
+    </html>
+  );
+}
