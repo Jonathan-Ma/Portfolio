bff90808b1653caede23689fb826fb67c4116ff8
JAM <jonathan.ma97@outlook.com>
2024-05-29 19:31:02 -0400

    basically done with header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-
+"use client";
+import { links } from "@/lib/data";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 export default function Header() {
   return (
     <header className='z-[999] relative'>
-        <div className= "fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950dark:border-black/40 dark:bg-opacity-75 ">
-        </div>
-    </header>
+      <motion.div className="fixed top-0 left-1/2 h-[4.5rem] w-full border backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full" initial={{ y: -100, x: "-50%", opacity: 0 }} animate={{ y: 0, x: "-50%", opacity: 1 }}><ul className="flex text-[0.9rem] h-full justify-center items-center font-medium text-white gap-3 flex-wrap md:flex-nowrap">
+          {
+            links.map(link => (
+              <motion.li className="py-1 px-2" key={link.hash}
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}>
+                <Link className="justify-center items-center hover:text-rose-400" href={link.hash}>
+                  {link.name}
+                </Link>
+              </motion.li>
+            ))
+          }
+        </ul>
+      </motion.div>
+    
+    </header >
   )
 }
