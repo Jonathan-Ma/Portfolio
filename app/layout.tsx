7c5a7302ab2cdf0ba25ed5d0b513ea1e0562f6ce
JAM <jonathan.ma97@outlook.com>
2024-05-28 23:02:44 -0400

    got the header up and intro image ok for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Header from "@/components/header";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,17 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-gray-950`}>
-        {/* <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.25rem] "></div> */}
-        {/* <div className="sm:w-[200rem] m:w-[90rem]">
-          <img src="./B33LRGB_fb.png" className=" w-full h-full opacity-75  "/>
-        </div>
-        <div className="absolute bottom-0 w-full h-[10rem] bg-gradient-to-t from-black"></div> */}
-        <div className="relative style=w-500px left-400px md:{relative min-h-screen flex}">
-          <img src="./B33LRGB_fb.png" className="absolute object-cover object-right md: {w-full h-full object-cover} opacity-75" alt="Intro Image" />
+      <body className={`${inter.className} bg-black text-gray-950 h-[5000px]`}>
+        <div className="relative md:{relative min-h-screen flex}">
+          <img src="./B33LRGB_fb.png" className="absolute object-right h-full object-cover md:object-cover md:w-full md:h-full opacity-85" alt="Intro Image" />
           <div className="absolute bottom-0 w-full h-[10rem] bg-gradient-to-t from-black to-transparent"></div>
         </div>
+        <Header />
         {children}
       </body>
     </html>
