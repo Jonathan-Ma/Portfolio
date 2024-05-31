e422b15cc8c6b9e22f55af7207293359d69e1536
JAM <jonathan.ma97@outlook.com>
2024-05-31 15:51:13 -0400

    minor tweak to main div

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
-    <main className="z-[100] font-sans w-full top-[0rem] absolute flex flex-col items-center justify-center">
+    <main className="z-[100] font-sans w-full h-full top-[0rem] absolute flex flex-col items-center justify-center">
       <Intro/>
       <SectionDivider/>
       <About/>
--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 
 export default function Intro() {
     return (
-        <section className='text-center mt-[8rem]'>
+        <section className='text-center mt-[5rem]'>
             <motion.div className='flex items-center justify-center' initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", damping: 10, duration: 0.2, delay: 0.5 }}>
                 <Image src={'/DHPortraitThumb.jpg'} alt={'DHPortrait'} width="192" height="192" quality='100' priority={true} className='h-[10rem] w-[10rem] rounded-full border-[0.35rem] object-cover' />
             </motion.div>
