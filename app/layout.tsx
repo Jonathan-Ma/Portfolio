b4975fe57e5d4c56409bed592e8542a6206a5a2e
JAM <jonathan.ma97@outlook.com>
2024-05-31 23:13:31 -0400

    i feel like im breaking it

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-black text-blue-50 h-[5000px]`}>
         <div className="relative min-h-screen flex -z-[100]">
-          <img src="./B33LRGB_fb.png" className="absolute object-right h-full object-cover md:object-cover md:w-full md:h-full opacity-60" alt="Intro Image" />
-          <div className="absolute bottom-0 w-full h-[15rem] bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute inset-0 bg-cover bg-no-repeat" style={{ backgroundImage: "url('/B33LRGB_fb.png')", backgroundPosition: "right center", backgroundAttachment: "fixed" }}
+          ></div>
+          <div className="absolute bottom-0 w-full h-[30rem] bg-gradient-to-t from-black to-transparent"></div>
         </div>
         <Header />
         {children}
