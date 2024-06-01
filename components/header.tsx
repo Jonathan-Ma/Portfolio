d95e4578d48e6cad9f63886ba85fc1cf3c9a1a50
JAM <jonathan.ma97@outlook.com>
2024-05-31 23:22:59 -0400

    fixing the header blur

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,16 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className='z-[999] relative'>
-      <motion.div className="flex fixed top-0 left-1/2 justify-center h-[4rem] w-full border backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full" initial={{ y: -100, x: "-50%", opacity: 0 }} animate={{ y: 0, x: "-50%", opacity: 1 }}>
+      <motion.div
+      className="flex fixed top-0 left-1/2 justify-center h-[4rem] w-full border backdrop-blur-[0.2rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+      initial={{ y: -100, x: "-50%", opacity: 0 }}
+      animate={{ y: 0, x: "-50%", opacity: 1 }}
+      style={{
+        //backgroundColor: 'rgba(255, 255, 255, 0.3)', // Fallback for browsers that do not support backdrop-filter
+        backdropFilter: 'blur(0.5rem)',
+        WebkitBackdropFilter: 'blur(0.5rem)', // Ensure support for Webkit browsers
+      }}
+    >
         <ul className="flex text-[0.9rem] h-full justify-center items-center font-medium text-white sm:gap-3 flex-wrap md:flex-nowrap">
           {
             links.map(link => (
@@ -21,7 +30,6 @@ export default function Header() {
           }
         </ul>
       </motion.div>
-
     </header >
   )
 }
