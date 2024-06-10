0e86e8c580f5a95f5e57857244c0066c61c40bed
JAM <jonathan.ma97@outlook.com>
2024-06-10 12:38:23 -0400

    intro changed

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,9 +7,15 @@ export default function Intro() {
     return (
         <div className='flex text-center h-screen items-center'>
             <section>
-                <motion.p className='flex flex-wrap sm:w-[40rem] sm:text-2xl font-medium pt-[1rem] justify-center' initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", damping: 10, duration: 0.2, delay: 0.7 }}>
+                <motion.p className='flex flex-wrap sm:w-[40rem] sm:text-2xl font-medium justify-center' initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", damping: 10, duration: 0.2, delay: 0.7 }}>
                     <span className='font-bold text-2xl sm:text-5xl'>Hi there!</span>
-                    <br />Welcome to the junkyard of my mind! Here, you&apos;ll find a mishmash of everything that goes on in my mind and projects that I have worked on and also WIP.
+                    <br />
+                        Welcome to the junkyard of my mind! Here, you&apos;ll find a mishmash of everything that goes on in my mind and projects that I have worked on and also WIP.
+                    <br />
+                    <br />
+                    <span>
+                        PS: I&apos;m working on this website too! so come back often and check to see if it's updated :&#41;
+                    </span>
                 </motion.p>
             </section>
         </div>
