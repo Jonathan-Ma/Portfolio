ca951255def7ffce74ad191aeaca240a7985733e
JAM <jonathan.ma97@outlook.com>
2024-06-11 15:09:55 -0400

    need to be more productive

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 export default function About() {
   return (
-    <div className='flex h-screen items-center'>
+    <div className='flex w-screen h-screen items-center justify-center bg-neutral-400 relative bg-gradient-to-b from-black to-transparent'>
       <section className='text-center' id="about">
         <motion.div className='flex items-center justify-center mb-[2rem]' initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", damping: 10, duration: 0.2, delay: 0.5 }}>
           <Image src={'/DHPortraitThumb.jpg'} alt={'DHPortrait'} width="192" height="192" quality='100' priority={true} className='h-[8rem] w-[8rem] sm:h-[12rem] sm:w-[15rem] rounded-full border-[0.35rem] object-cover' />
@@ -13,7 +13,7 @@ export default function About() {
         <motion.p className='flex flex-wrap w-[15rem] sm:w-[40rem] sm:text-2xl font-medium justify-center' initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "spring", damping: 10, duration: 0.2, delay: 1.0 }}>
           <span className='font-bold text-l sm:text-5xl'>About me</span>
          
-          My name is Jonathan, I am a graduate from the University of Hawaii with a Bachelors in CS.
+          I'm Jonathan, a CS graduate from the University of Hawaii. I've spent time living in Georgia, China, and Hawaii, which has been a great experience!
         </motion.p>
       </section>
     </div>
