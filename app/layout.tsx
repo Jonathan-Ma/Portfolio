1e34d3e02fb7f52b3a839ec8f71257021d55077b
JAM <jonathan.ma97@outlook.com>
2024-06-15 19:14:56 -0400

    added speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Header from "@/components/header";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Header />
         {children}
         <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
