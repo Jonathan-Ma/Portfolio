6c89766fe2327e871783a437c14bdac26ec06e3b
Jonathan <jonathanma@gotenna.com>
2024-09-20 17:16:03 -0400

    added widget, need the icons

--- components/WeatherWidget.tsx
+++ components/WeatherWidget.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function WeatherWidget() {
     return (
-        <div className={'flex absolute top-0 right-0 h-[10rem] w-[10rem]'}><h3>27</h3></div>
+        <div
+            className={'flex flex-col flex-wrap absolute top-[5rem] right-[7rem] h-[10rem] w-[10rem] rounded-lg backdrop-blur-[0.2rem] backdrop-brightness-90 justify-center content-center'}>
+            <div></div>
+            <div>45</div>
+            <div>45</div>
+            <div>45</div>
+        </div>
     );
 }
 
